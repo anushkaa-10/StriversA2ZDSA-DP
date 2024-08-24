478a31d9c38bb6d1c83e4bb029a9893135174eef
anushkaa-10 <138236542+anushkaa-10@users.noreply.github.com>
2024-08-24 09:58:01 +0530

    Create Q1.Ninjas-Training.cpp

---
+++ DP on grids/Q1.Ninjas-Training.cpp
@@ -0,0 +1,27 @@
+int solve(int day, int last, vector<vector<int>> &points, vector<vector<int>> &dp){
+    if(day==0){
+        int maxi = 0;
+        for (int task = 0; task < 3; task++) {
+          if (task != last)
+            maxi = max(maxi, points[0][task]);
+        }
+        return maxi;
+    }
+    if(dp[day][last]!=-1) return dp[day][last];
+    int maxi = 0;
+
+    for (int task = 0; task < 3; task++){
+        if(task!=last){
+            int pnt = points[day][task] + solve(day-1,task,points,dp);
+            maxi = max(maxi,pnt);
+        }
+    }  
+    return dp[day][last] = maxi;
+}
+
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    // Write your code here.
+    vector<vector<int>> dp(n,vector<int>(4,-1));
+    return solve(n-1,3,points,dp);
+}
