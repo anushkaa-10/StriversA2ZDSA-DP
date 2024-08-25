323791e8bd41e096a48a889291483949bae3d4d9
anushkaa-10 <138236542+anushkaa-10@users.noreply.github.com>
2024-08-25 21:40:12 +0530

    Create Q1.SubsetSumequaltoK.cpp

---
+++ DP on subsequences/Q1.SubsetSumequaltoK.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+bool solve(int i, int k, vector<int> &arr,vector<vector<int>> &dp){
+    if(k==0) return true;
+    if(i==0) return arr[0]==k;
+
+    if(dp[i][k]!=-1) return dp[i][k];
+
+    bool notTake = solve(i-1,k,arr,dp);
+    bool take = false;
+    if(arr[i]<=k) take = solve(i-1,k-arr[i],arr,dp);
+    return dp[i][k]=take | notTake;
+
+}
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    // Write your code here.
+    vector<vector<int>> dp(n,vector<int>(k+1,-1));
+    return solve(n-1,k,arr,dp);
+}
